70ccbc0e531f13d483b118a4993722d1f426f0a0
WD-Projects <akibmahir6@gmail.com.com>
2025-01-18 03:16:20 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      screens: {
+        sm: "600px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 640px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
-    plugins: [],
-  }
+  },
+};
