85ead5321d9f66992ded3ed6e84fe097b2c46a6d
codeseeker <akibmahir6@gmail.com>
2025-02-14 00:52:10 +0600

    new added section 3 and 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         lg: "1024px",
         // => @media (min-width: 1024px) { ... }
 
-        desktop: "1280px",
+        xl: "1280px",
         // => @media (min-width: 1280px) { ... }
       },
     },
